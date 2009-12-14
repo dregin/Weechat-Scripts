d430ad7eed08ade2490d19c00a1cfaee3a136881
Bernard McKeever <dregin@gmail.com>
2009-12-15 00:23:24 +0100

    Setup the file to look a little more like an actual script.

--- rb_online.py
+++ rb_online.py
@@ -5,7 +5,7 @@ SCRIPT_LICENSE = "GLP3"
 SCRIPT_DESC = "Colour nicks in #lobby depending on whether the user is logged into redbrick or not"
 
 import os
-
+import_ok = true
 try:
 	import weechat
 except:
@@ -13,5 +13,12 @@ except:
 	print "Get weechat @ http://www.weechat.org"
 	import_ok = false
 
+def lobby_users():
 
+def users_online():
 
+def colour_nicks():
+
+if __name__ == "__main__" and import_ok:
+	if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
+		
