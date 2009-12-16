5dd27f4ed74406688a9d94ec478082e56ddf680e
Bernard McKeever <dregin@gmail.com>
2009-12-16 01:18:19 +0100

    Bare bones of the split squish script. Actually loads into weechat \o/

--- split_squash.py
+++ split_squash.py
@@ -1,4 +1,23 @@
-import weechat
+SCRIPT_NAME = "Split_Squish"
+SCRIPT_AUTHOR = "Bernard McKeever <dregin@gmail.com>"
+SCRIPT_VERSION = "0.1"
+SCRIPT_LICENSE = "GLP3"
+SCRIPT_DESC = "Colour nicks in #lobby depending on whether the user is logged into redbrick or not"
 
-wechat.register("split_squash", "Dregin", "0.1", "GPL3", "Collapses netsplit spam into one line", "", "")
-weechat.prnt("","Hello from Split Squash")
+import os
+import_ok = True
+
+try:
+        import weechat
+except:
+        print "This script must be run under Weechat"
+        print "Get weechat @ http://www.weechat.org"
+        import_ok = False
+
+def check_split():
+        return True     
+
+if __name__ == "__main__" and import_ok:
+	if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
+		weechat.prnt("", "YEOW!")
+		hook = weechat.hook_signal("*, irc_in_quit", "check_split", "")
