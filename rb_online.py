f55a1fef03a687e7fbddca92f1ba1a785d153f74
Bernard McKeever <dregin@gmail.com>
2009-12-30 04:47:07 +0100

    a dictionary of all users logged into the users server

--- rb_online.py
+++ rb_online.py
@@ -7,6 +7,8 @@ SCRIPT_DESC = "Colour nicks in #lobby depending on whether the user is logged in
 import os
 import_ok = True
 PRINT_CMD = "rbusers"
+cmd_buffer = ""
+my_dict = {}
 
 try:
 	import weechat
@@ -17,12 +19,20 @@ except:
 
 def lobby_users():
 	print ""
+	return weechat.WEECHAT_RC_OK
 
 def users_online():
+	value = 0
 	pipe = os.popen('users')
-	pipeout = pipe.read()
-	users_online = pipeout.split(" ")	# Splitting output of 'users' into a list:	
+	pipeout = pipe.read()	
+	for item in pipeout.split():
+		key = item
+		if my_dict.get( key ):
+			my_dict[ key ] += int( value )
+		else:
+			my_dict[ key ] = int( value )
 	pipe.close()				# Optimize list - remove duplicates? Performance?
+	users_online = my_dict
 	return users_online			# users_online is a list
 
 def colour_nicks():
@@ -30,9 +40,11 @@ def colour_nicks():
 
 def print_users(data, buffer, args):
 	users = users_online()
-	weechat.prnt("", "Users online: %s" % users)
+	cmd_buffer = buffer
+	for name in users:
+		weechat.prnt(cmd_buffer, "%s" % name)
 	return weechat.WEECHAT_RC_OK
 
 if __name__ == "__main__" and import_ok:
 	if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
-		weechat.hook_command("rbusers", "Print all Redbrick users logged in.", "", "", "", "print_users", "" )		
+		weechat.hook_command("rbusers", "Print all Redbrick users logged in.", "", "", "", "print_users", "" )
